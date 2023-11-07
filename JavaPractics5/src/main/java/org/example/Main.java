e4e7863d59192d01dae49558224a8dc2a2c1bebb
Denis Martynov <idenismartynov@yandex.ru>
2023-11-07 18:05:28 +0300

    Решал задачки

---
+++ JavaPractics5/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Task task = new Task();
+//        task.randomPhrase();
+
+//        Task2 task2 = new Task2();
+//        task2.PoolPuzzleOne();
+
+        Task3 task3 = new Task3();
+        task3.consumptionCounter();
+        task3.countTotalConsumption();
+    }
+}
