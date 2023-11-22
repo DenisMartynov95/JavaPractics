69782705b29d7f3aa36307ee87844a977b660c40
Denis Martynov <idenismartynov@yandex.ru>
2023-11-22 15:30:37 +0300

    Начал решать головоломку

--- JavaPractics7/src/main/java/org/task1/GameLaunch.java
+++ JavaPractics7/src/main/java/org/task1/GameLaunch.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.task1;
 
 public class GameLaunch {
     public static void main(String[] args) {
---
+++ JavaPractics7/src/main/java/task2/Echo.java
@@ -0,0 +1,8 @@
+package task2;
+
+public class Echo {
+    int ... = 0
+    void ... {
+        System.out.println("привееееет...");
+    }
+}
